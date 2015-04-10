ea4d51a0445da1d143bc84b874605f806144c982
Garrigos Fernando <garrigos.fernando@etu.unice.fr>
2015-04-10 17:42:08 +0200

    implementation web service PrendreRendezvous

--- j2e/src/main/java/fr/unice/polytech/se/demo/webservice/ServicePrendreRendezvousBean.java
+++ j2e/src/main/java/fr/unice/polytech/se/demo/webservice/ServicePrendreRendezvousBean.java
@@ -1,15 +1,41 @@
 package fr.unice.polytech.se.demo.webservice;
 
+import fr.unice.polytech.se.demo.domain.impl.ManageFranchise;
+import fr.unice.polytech.se.demo.domain.impl.PrendreRendezvous;
+import fr.unice.polytech.se.demo.entities.magasin.Magasin;
+import fr.unice.polytech.se.demo.entities.rendezvous.Rendezvous;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by Garrigos Fernando on 10/04/15.
  */
-public class ServicePrendreRendezvousBean {
-    private static ServicePrendreRendezvousBean ourInstance = new ServicePrendreRendezvousBean();
+@WebService(targetNamespace = "http://www.polytech.unice.fr/servicePrendreRendezvous")
+@Stateless(name = "ServicePrendreRendezvous")
+public class ServicePrendreRendezvousBean implements ServicePrendreRendezvous{
 
-    public static ServicePrendreRendezvousBean getInstance() {
-        return ourInstance;
+    @EJB
+    private PrendreRendezvous prv;
+
+
+    @WebMethod
+    @Override
+    public boolean prendreRendezvous(Magasin m, Date date) {
+
+        if (prv.prendreRendezvous(m,date)) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
-    private ServicePrendreRendezvousBean() {
+    @WebMethod
+    @Override
+    public List<Rendezvous> getListeRendezvous() {
+        return prv.getRendezvous();
     }
 }
