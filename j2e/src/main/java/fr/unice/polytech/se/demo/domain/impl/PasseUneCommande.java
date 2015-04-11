a2a4b1baf1890f2956eb389d2965e22743981192
Garrigos Fernando <garrigos.fernando@etu.unice.fr>
2015-04-11 16:44:06 +0200

    ajout interface passeUneCommande

---
+++ j2e/src/main/java/fr/unice/polytech/se/demo/domain/impl/PasseUneCommande.java
@@ -0,0 +1,22 @@
+package fr.unice.polytech.se.demo.domain.impl;
+
+import fr.unice.polytech.se.demo.entities.magasin.Magasin;
+
+import javax.ejb.Remote;
+import java.util.List;
+
+/**
+ * Created by Garrigos Fernando on 11/04/15.
+ */
+@Remote
+
+public interface PasseUneCommande {
+
+        public boolean passeUneCommande(Double prixHT,String Rendezvous,String cookies);
+
+        public String choisirCookies();
+
+        public List<Magasin> getMagasin();
+
+    }
+}
